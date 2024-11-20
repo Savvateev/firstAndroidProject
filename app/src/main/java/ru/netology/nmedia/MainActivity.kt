c98ae7ca1b930c953a83c6698f9dc7706d6310da
Pavel Savvateev <savvateev@icloud.com>
2024-11-20 10:05:27 +0800

    layout fixed

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -20,9 +20,10 @@ fun toView(count : Int) : String {
     return when (count) {
         in 1..999 -> count.toString()
         in 1000..1099 -> "1K "
-        in 1100..999999 -> ((count.toFloat()/1000).toBigDecimal().setScale(1,RoundingMode.DOWN).toString()) + "K "
+        in 1100..10000 -> ((count.toFloat()/1000).toBigDecimal().setScale(1,RoundingMode.DOWN).toString()) + "K "
+        in 10001..999999 -> ((count.toFloat()/1000).toBigDecimal().setScale(0,RoundingMode.DOWN).toString()) + "K "
         in 1000000..1099000 -> "1M "
-        in 1100000..100000000 -> ((count.toFloat()/1000000).toBigDecimal().setScale(1, RoundingMode.DOWN)).toString() + "M "
+        in 1100000..100000000 -> ((count.toFloat()/1000000).toBigDecimal().setScale(0, RoundingMode.DOWN)).toString() + "M "
         else -> "0"
     }
 }
